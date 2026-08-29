4c7d4b7125e594fdd50e625a10b2a197e0c38ec7
keshav62 <keshavsing1230@gmail.com>
2026-08-29 05:30:10 +0530

    feat: add useState hook to WorkerProfile component

--- civic-connect/src/pages/worker/WorkerProfile.jsx
+++ civic-connect/src/pages/worker/WorkerProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import { Button } from '../../components/common/Button';
